c07ac422768d82573bc699ee9716102970ab8adf
hokwaichan-gov <hokwai.chan@honolulu.gov>
2026-01-16 14:27:34 -1000

    Created timesheet page

---
+++ timesheet/resources/views/timesheet.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Timesheet Page</h1>
+</x-layout>
--- timesheet/routes/web.php
+++ timesheet/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/timesheet', function () {
+    return view('timesheet');
 });
