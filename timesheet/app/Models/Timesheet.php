13680244fafc7c208d60cd634cc6acf066dd48a7
hokwaichan-gov <hokwai.chan@honolulu.gov>
2026-01-16 15:23:52 -1000

    Replaced array with model

--- timesheet/app/Models/Timesheet.php
+++ timesheet/app/Models/Timesheet.php
@@ -2,42 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Timesheet
+class Timesheet extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'date' => '1/16/2025',
-                'startTime' => '7:45 am',
-                'endTime' => '4:30 pm',
-            ],
-            [
-                'id' => 2,
-                'date' => '1/17/2025',
-                'startTime' => '7:45 am',
-                'endTime' => '4:30 pm',
-            ],
-            [
-                'id' => 3,
-                'date' => '1/18/2025',
-                'startTime' => '7:45 am',
-                'endTime' => '4:30 pm',
-            ],
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $timesheet = Arr::first(static::all(), fn($timesheet) => $timesheet['id'] == $id);
-
-        if (! $timesheet) {
-            abort(404);
-        }
-
-        return $timesheet;
-    }
+    protected $table = 'timesheet_listings';
+    protected $fillable = ['date', 'startTime', 'endTime'];
 }
