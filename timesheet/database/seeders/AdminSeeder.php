025cc1a3588eaabb97ad99769a5dcd8ab849e627
hokwaichan-gov <hokwai.chan@honolulu.gov>
2026-01-21 14:55:17 -1000

    Created user_type

---
+++ timesheet/database/seeders/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'user_type' => 'admin', // Assign the user type as 'admin'
+        ]);
+    }
+}
